e12e7d996d2639e85b9c63271fcf553968603b2e
yitian <yitian.z@foxmail.com>
2020-06-26 23:01:24 +0800

    update

--- src/main/java/cn/zyt/springbootlearning/SpringbootLearningApplication.java
+++ src/main/java/cn/zyt/springbootlearning/SpringbootLearningApplication.java
@@ -4,10 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Repository;
 
 @SpringBootApplication(scanBasePackages = {"cn.zyt.springbootlearning.*"})
